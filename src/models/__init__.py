6296664e5bba86b19c6bd06714c33bed6967717f
mrh0wl <h0wl.jacket@gmail.com>
2023-01-05 15:46:08 -0600

    import new Model

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,4 +1,4 @@
-from .payment_model import Payment, Plan
+from .payment_model import Payment, Plan, TotalPay
 from .platform_model import PlatformModel, Platforms
 from .subscription_model import Subscription
 from .user_model import User
